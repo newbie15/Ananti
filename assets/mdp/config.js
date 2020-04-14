1412c0c1d0471d79d6923187cc821c9be7f7c6e6
maintenance GSDI <fajar.rukmo@gmail.com>
2020-04-14 11:10:32 +0700

    Update
    Update

--- assets/mdp/config.js
+++ assets/mdp/config.js
@@ -14,7 +14,6 @@ var DEBUG = 1;
 
 var BASE_URL = "";
 
-<<<<<<< HEAD
 if (CUSTOM == 1) {
 	var url = window.location.href;
 	console.log(url);
@@ -33,24 +32,4 @@ if (CUSTOM == 1) {
 	} else {
 		var BASE_URL = "http://10.23.3.101/MDP/";
 	}
-=======
-if(CUSTOM == 1) {
-    var url = window.location.href;
-    console.log(url);
-
-    var x = url.split("/");
-    console.log(x);
-
-    var BASE_URL = x[0]+"//"+x[2]+"/"+x[3]+"/"; // "http://10.23.3.101/MDP/";
-    console.log(BASE_URL);
-
-}else{
-    if (DEBUG == 2) {
-        var BASE_URL = "http://167.99.64.44/Ananti/";
-    } else if (DEBUG == 1) {
-        var BASE_URL = "http://localhost/ananti/";
-    } else {
-        var BASE_URL = "http://10.23.3.101/MDP/";
-    }
->>>>>>> development
 }
