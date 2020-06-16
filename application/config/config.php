e1987b2e3d7e6a4fd386bab234a516e734839394
Mill GSPP <fajar.rukmo@gmail.com>
2020-06-16 10:01:37 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ananti/';
+$config['base_url'] = 'http://192.168.1.10/ananti/';
 // $config['base_url'] = 'http://192.168.0.6/MDP/';
 
 /*
