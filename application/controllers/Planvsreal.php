26a683cbd5a333dc777050855be88e4fd75c0650
Mill GSPP <fajar.rukmo@gmail.com>
2020-07-13 10:40:32 +0700

    update

--- application/controllers/Planvsreal.php
+++ application/controllers/Planvsreal.php
@@ -299,13 +299,13 @@ class Planvsreal extends CI_Controller {
 		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 			// echo 'This is a server using Windows!';
 			include APPPATH.'third_party\PHPExcel.php';
-		$fe = "template_planvsreal.xlsx";
+		$fe = "template_planvsreal.xls";
 		$filex = dirname(__FILE__) .'\..\..\assets\excel\\'.$fe;
 
 		} else {
 			// echo 'This is a server not using Windows!';
 			include APPPATH.'third_party/PHPExcel.php';
-		$fe = "template_planvsreal.xlsx";
+		$fe = "template_planvsreal.xls";
 		$filex = dirname(__FILE__) .'/../../assets/excel/'.$fe;
 
 		}
