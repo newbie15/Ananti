7ce7f72be1c19d889375f0c3e6d4f5fe830f7ddb
Fajar Anggoro <fajar.rukmo@gmail.com>
2021-02-05 11:17:25 +0700

    create controller

---
+++ application/controllers/Job_aid/J11.php
@@ -0,0 +1,248 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class J111 extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		// $this->load->library('grocery_CRUD');
+	}
+	
+	public function index()
+	{
+		$query = $this->db->query("SELECT nama,comment FROM aux_job_aid WHERE nomor = 'J11';");
+		foreach ($query->result() as $row)
+		{
+			$output['comment'] = $row->comment;
+			$header['title'] = "J11 - ".$row->nama;
+		}
+
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j11.js"),
+		];
+		
+		$output['content'] = '';
+
+		$output['pdf'] = "yes";
+		$output['js'] = base_url("assets/pdfobject/pdfobject.min.js");
+		$output['dokumen'] = "emp";
+		$output['filename'] = "J11.pdf";
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j11',$output);
+		$this->load->view('footer',$footer);
+	}
+
+	public function a0()
+	{
+
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j11-a0.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j11-a0',$output);
+		$this->load->view('footer',$footer);		
+	}
+
+	public function a14()
+	{
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j11-a14.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j11-a14',$output);
+		$this->load->view('footer',$footer);		
+	}
+	
+	public function a15()
+	{
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j11-a15.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j11-a15',$output);
+		$this->load->view('footer',$footer);		
+	}	
+
+}
---
+++ application/controllers/Job_aid/J62.php
@@ -0,0 +1,362 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class J62 extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		// $this->load->library('grocery_CRUD');
+	}
+	
+	public function index()
+	{
+		$query = $this->db->query("SELECT nama,comment FROM aux_job_aid WHERE nomor = 'J62';");
+		foreach ($query->result() as $row)
+		{
+			$output['comment'] = $row->comment;
+			$header['title'] = "J62 - ".$row->nama;
+		}
+
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j62.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j62',$output);
+		$this->load->view('footer',$footer);
+	}
+
+	public function load()
+	{
+		$id_pabrik = $_REQUEST['id_pabrik'];
+		$id_station = $_REQUEST['id_station'];
+		$tanggal = $_REQUEST['y']."-".$_REQUEST['m']."-".$_REQUEST['d'];		
+		$query = $this->db->query("SELECT unit,acm,keterangan FROM m_acm where id_pabrik = '$id_pabrik' AND id_station = '$id_station' AND tanggal='$tanggal';");
+
+		$i = 0;
+		$d = [];
+		foreach ($query->result() as $row)
+		{
+			// $d[$i][0] = $row->nama; // access attributes
+			$d[$i][0] = $row->unit; // or methods defined on the 'User' class
+			$d[$i][1] = $row->acm; // or methods defined on the 'User' class
+			$d[$i++][2] = $row->keterangan; // or methods defined on the 'User' class
+			// $d[$i][2] = $row->jenis_breakdown; // or methods defined on the 'User' class
+			// $d[$i++][3] = $row->jenis_problem; // or methods defined on the 'User' class
+		}
+		echo json_encode($d);
+	}
+
+	public function simpan()
+	{
+		$pabrik = $_REQUEST['pabrik'];
+		$station = $_REQUEST['station'];
+		$tanggal = $_REQUEST['y']."-".$_REQUEST['m']."-".$_REQUEST['d'];
+		$this->db->query("DELETE FROM `m_acm` where id_pabrik = '$pabrik' AND id_station = '$station' AND tanggal = '$tanggal' ");
+		$data_json = $_REQUEST['data_json'];
+		$data = json_decode($data_json);
+		foreach ($data as $key => $value) {
+			// $this->db->insert
+			$data = array(
+				'tanggal' => $tanggal,
+				'id_pabrik' => $pabrik,
+				'id_station' => $station,
+				'unit' => $value[0],
+				'acm' => $value[1],
+				'keterangan' => $value[2],
+
+				// 'jenis_problem' => $value[2],
+				// 'jenis_breakdown' => $value[3],
+				// 'date' => 'My date'
+			);
+			// print_r($data);
+			if($value[0]!=""){
+				$this->db->insert('m_acm', $data);
+			}
+		}
+	}
+
+	public function load_problem(){
+		// SELECT max(tanggal) as tanggal,id_station,unit,acm FROM `m_acm` where acm = 1 GROUP by unit
+		$id_pabrik = $_REQUEST['id_pabrik'];
+
+		$query = $this->db->query("SELECT max(tanggal) as tanggal,id_station,unit,acm,keterangan FROM `m_acm` where acm = 0 AND id_pabrik = '$id_pabrik' GROUP by unit");
+
+		$i = 0;
+		$d = [];
+		foreach ($query->result() as $row)
+		{
+			$d[$i][0] = $row->tanggal; 
+			$d[$i][1] = $row->id_station; 
+			$d[$i][2] = $row->unit;
+			$d[$i][3] = $row->acm;
+			$d[$i++][4] = $row->keterangan;
+		}
+		echo json_encode($d);
+	}
+
+	public function problem()
+	{
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			// base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			// base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/mdp/acm_problem.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('content-acm-problem',$output);
+		$this->load->view('footer',$footer);
+	}
+
+	public function a0()
+	{
+
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j1-a0.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j1-a0',$output);
+		$this->load->view('footer',$footer);		
+	}
+
+	public function a14()
+	{
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j1-a14.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j1-a14',$output);
+		$this->load->view('footer',$footer);		
+	}
+	
+	public function a15()
+	{
+		$output['content'] = "test";
+		$output['main_title'] = "Data Avaibility Cricital Machine";
+		
+		$header['title'] = "ACM";
+		$header['css_files'] = [
+			base_url("assets/jexcel/css/jquery.jexcel.css"),
+			base_url("assets/jexcel/css/jquery.jcalendar.css"),
+		];
+
+		$footer['js_files'] = [
+			// base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js'),
+			base_url("assets/jexcel/js/jquery.jexcel.js"),
+			base_url("assets/jexcel/js/jquery.jcalendar.js"),
+			base_url("assets/mdp/config.js"),
+			base_url("assets/mdp/global.js"),
+			base_url("assets/job_aid/j1-a15.js"),
+		];
+		
+		$output['content'] = '';
+		
+		$nama_pabrik = $this->session->user;
+		$kategori = $this->session->kategori;
+
+		$query = $this->db->query("SELECT nama FROM master_pabrik;");
+
+		$output['dropdown_pabrik']= "";
+		if($kategori<2){
+			$output['dropdown_pabrik']= "<select id=\"pabrik\">";
+		}else{
+			$output['dropdown_pabrik']= "<select id=\"pabrik\" disabled>";
+		}
+		
+		foreach ($query->result() as $row)
+		{
+			if($nama_pabrik==$row->nama){
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option selected=\"selected\">".$row->nama."</option>";
+			}else{
+				$output['dropdown_pabrik'] = $output['dropdown_pabrik']."<option>".$row->nama."</option>";
+			}
+		}
+		$output['dropdown_pabrik'] .= "/<select>";		
+
+		$output['dropdown_station'] = "<select id=\"station\"></select>";
+
+
+		$this->load->view('header',$header);
+		$this->load->view('job_aid/content-j1-a15',$output);
+		$this->load->view('footer',$footer);		
+	}	
+
+}
